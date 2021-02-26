8082a82fbfe88e27eadb7a4e50d8549182aa17a2
Urexxx6x <76719341+Urexxx6x@users.noreply.github.com>
2021-02-26 14:15:20 +0300

    Delete modmail directory

--- modmail/database/databaseHandler.js
+++
@@ -1,6 +0,0 @@
-const config = require('../config.js')
-const mongoose = require('mongoose')
-mongoose.connect(config.databaseToken, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(console.log('Database Connected'))
